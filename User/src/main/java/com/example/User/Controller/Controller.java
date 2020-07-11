441ca731e46ec7d19a50f4d51f018f17098796be
Amitesh  Kumar Upadhyay <amiteshupadhyay01@gmail.com>
2020-07-11 11:45:04 +0530

    Update Controller.java

--- User/src/main/java/com/example/User/Controller/Controller.java
+++ User/src/main/java/com/example/User/Controller/Controller.java
@@ -12,11 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.User.DTO.UserDTO;
 import com.example.User.Service.IUserService;
 import com.example.User.VO.UserDetails;
-
 
 @RestController
 @RequestMapping("/user")
